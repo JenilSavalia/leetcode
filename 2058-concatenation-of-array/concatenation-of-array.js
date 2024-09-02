8248b1cc34fcbcde61f395296c3bdb9a12cfc0ff
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-09-02 14:34:59 +0530

    Time: 66 ms (54.59%) | Memory: 51.6 MB (77.44%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -4,15 +4,7 @@
  */
  nums = [1,2,1]
 var getConcatenation = function(nums) {
-    let temp = []
-    for(i=0;i<nums.length;i++){
-        
-        temp.push(nums[i])
-    }
-     for(i=0;i<nums.length;i++){
-        
-        temp.push(nums[i])
-    }
-
-    return temp;
+    let ans = nums.concat(nums);
+   return ans;
+    
 };
