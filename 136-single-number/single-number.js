27453c3225a40d3bcd542572fedad54778b31e5b
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-10-17 21:27:43 +0530

    Time: 17 ms (100.00%) | Memory: 54.3 MB (16.90%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+var singleNumber = function(nums) {
+
+let obj = {};
+
+for(i=0;i<nums.length;i++){
+    
+if(!obj.hasOwnProperty(nums[i])){
+    obj[nums[i]] = 1;
+}
+else{
+    obj[nums[i]] = obj[nums[i]] + 1;
+}
+
+}
+
+for(let x in obj){
+    if(obj[x] == 1){
+        
+        return Number(x);
+    }
+}
+};
