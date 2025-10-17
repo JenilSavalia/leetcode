b0e2d872a3fc4cc6509457e5baa4bdc8da35eadb
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-10-17 22:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (37.92%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+
+        // 1. Hash map based approach O(nlogn)
+
+        // int n = nums.size();
+        // map<int, int> m;
+        // for (int i = 0; i <= n; i++) {
+        //     m.insert({i, 0});
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     m[nums[i]]++;
+        // }
+
+        // int retValue = 0;
+        // for (auto x : m) {
+        //     cout << x.second << endl;
+        //     if (x.second == 0) {
+        //         retValue = x.first;
+        //         break;
+        //     }
+        // }
+        // return retValue;
+
+        // 2. sum of first n natural numbers O(n)
+
         int n = nums.size();
-        map<int, int> m;
-        for (int i = 0; i <= n; i++) {
-            m.insert({i, 0});
-        }
-        for (int i = 0; i < n; i++) {
-            m[nums[i]]++;
+        int sum = n * (n + 1) / 2;
+        int currSum = 0;
+
+        for (int x : nums) {
+            currSum += x;
         }
 
-        int retValue = 0;
-        for (auto x : m) {
-            cout << x.second << endl;
-            if (x.second == 0) {
-                retValue = x.first;
-                break;
-            }
-        }
-        return retValue;
+        return sum - currSum;
     }
 };
