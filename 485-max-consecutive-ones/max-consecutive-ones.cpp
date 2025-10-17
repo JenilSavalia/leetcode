90684d39c539faecf31d71d1d83becc9b997f9db
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-10-17 14:20:10 +0530

    Time: 3 ms (17.91%) | Memory: 50.1 MB (67.78%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
 
-        vector<int> zero;
-        zero.push_back(-1);
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                zero.push_back(i);
-                cout << i << endl;
-            }
-        }
-        zero.push_back(nums.size());
+        // vector<int> zero;
+        // zero.push_back(-1);
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] == 0) {
+        //         zero.push_back(i);
+        //         cout << i << endl;
+        //     }
+        // }
+        // zero.push_back(nums.size());
+
+        // int count = 0;
+        // for (int i = 1; i < zero.size(); i++) {
+        //     if (count < zero[i] - zero[i - 1]) {
+        //         int len = (zero[i] - zero[i - 1]) - 1;
+        //         count = max(len, count);
+        //     }
+        // }
+        // return count;
 
         int count = 0;
-        for (int i = 1; i < zero.size(); i++) {
-            if (count < zero[i] - zero[i - 1]) {
-                int len = (zero[i] - zero[i - 1]) - 1;
-                count = max(len, count);
+        int currentCount=0;
+        for (int x : nums) {
+            if (x == 1) {
+                currentCount++;
+            count = max(currentCount,count);
+            }else{
+                currentCount = 0;
             }
         }
         return count;
