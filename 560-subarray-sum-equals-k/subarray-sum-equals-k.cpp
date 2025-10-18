49982a7bffbc8de70b638e95c91080ddc8b94d3a
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-10-18 13:26:47 +0530

    Time: 2051 ms (21.41%) | Memory: 34.2 MB (99.13%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
-    int SumCount = 0;
-    int count = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        SumCount = nums[i];
-        if(nums[i]==k){
-            count++;
-        }
-        for (int j = i + 1; j < nums.size(); j++)
-        {
-            SumCount += nums[j];
-            if (SumCount == k)
-            {
+
+        // Brute Foce Approach
+        int SumCount = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            SumCount = nums[i];
+            if (nums[i] == k) {
                 count++;
             }
+            for (int j = i + 1; j < nums.size(); j++) {
+                SumCount += nums[j];
+                if (SumCount == k) {
+                    count++;
+                }
+            }
         }
-    }
-    return count;
+        return count;
 
+        // Using Better approach : PREFIX SUM using hashmap
+        // map<int, int> prefixSum;
+        // int len = 0;
+        // int sum = 0;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     sum += nums[i];
+        //     if (sum == k) {
+        //         len++;
+        //     }
+        //     int remaining = sum - k;
+        //     if (prefixSum.find(remaining) != prefixSum.end()) {
+        //         len++;
+        //     }
+        //     if (prefixSum.find(sum) == prefixSum.end()) {
+        //         prefixSum[sum] = i;
+        //     }
+        // }
+        // return len;
     }
 };
