9a39c5f0162bd9eb7c06d31330bdf40840f189a3
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-10-28 21:25:39 +0530

    Time: 75 ms (74.98%) | Memory: 88.7 MB (71.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,29 +2,56 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
 
-if(nums.size()==0) return 0;
+        // if (nums.size() == 0)
+        //     return 0;
 
-        map<int, int> mpp;
+        // map<int, int> mpp;
+        // for (int x : nums) {
+        //     mpp[x]++;
+        // }
+
+        // int count = 1;
+        // int maxCount = 1;
+
+        // int prev = mpp.begin()->first;
+        // for (auto x : mpp) {
+
+        //     if (x.first == prev + 1) {
+        //         count++;
+        //     } else {
+        //         maxCount = max(count, maxCount);
+        //         count = 1;
+        //     }
+        //     prev = x.first;
+        // }
+        // maxCount = max(maxCount, count);
+
+        // return maxCount;
+
+        // above method uses stl map which has O(log n) for insert, find, erase
+        // so overall complexicity for above algorithm is O(nlogn)
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+
+        unordered_set<int> stt;
         for (int x : nums) {
-            mpp[x]++;
+            stt.insert(x);
         }
 
-        int count = 1;
-        int maxCount = 1;
+        int longest = 1;
 
-        int prev = mpp.begin()->first;
-        for (auto x : mpp) {
-
-            if (x.first == prev + 1) {
-                count++;
-            } else {
-                maxCount = max(count, maxCount);
-                count = 1;
+        for (auto num : stt) {
+            int x = num;
+            int cnt = 1;
+            if (stt.find(num - 1) == stt.end()) {
+                while (stt.find(x + 1) != stt.end()) {
+                    x++;
+                    cnt++;
+                }
+                longest = max(longest, cnt);
             }
-            prev = x.first;
         }
-        maxCount = max(maxCount, count);
-
-        return maxCount;
+        return longest;
     }
 };
