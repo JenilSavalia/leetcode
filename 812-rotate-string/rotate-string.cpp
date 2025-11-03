3f96a75c97c0047f42909a59db759fcf94c5cbbb
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-03 12:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (35.77%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    void reverse(string& s, int start, int end) {
-        while (start < end) {
-            swap(s[start], s[end]);
-            start++;
-            end--;
-        }
-    }
+    // void reverse(string& s, int start, int end) {
+    //     while (start < end) {
+    //         swap(s[start], s[end]);
+    //         start++;
+    //         end--;
+    //     }
+    // }
 
     bool rotateString(string s, string goal) {
 
-        int k = s.length();
-        string temp = s;
-        for (int i = 0; i < k; i++) {
+        // int k = s.length();
+        // string temp = s;
+        // for (int i = 0; i < k; i++) {
 
-            reverse(s, 0, k - 1);
-            reverse(s, 0, k - i - 1);
-            reverse(s, k - i, k - 1);
+        //     reverse(s, 0, k - 1);
+        //     reverse(s, 0, k - i - 1);
+        //     reverse(s, k - i, k - 1);
 
-            if (s == goal) {
-                return true;
-            }
-            s = temp;
-        }
+        //     if (s == goal) {
+        //         return true;
+        //     }
+        //     s = temp;
+        // }
 
-        return false;
+        // return false;
+
+        // single liner approach
+        return s.size() == goal.size() && (s + s).find(goal) != string::npos;
     }
 };
