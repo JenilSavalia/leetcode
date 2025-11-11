a8affe053849eb9b89bb418b208f1cd2f46e3036
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-11 16:03:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (14.86%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,20 +10,49 @@
  * right(right) {}
  * };
  */
+
+// class Solution {
+//     void preorder(TreeNode* root, vector<int>& result) {
+//         if (root == NULL) {
+//             return;
+//         }
+//         result.push_back(root->val);
+//         preorder(root->left, result);
+//         preorder(root->right, result);
+//     }
+
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         preorder(root, result);
+//         return result;
+//     }
+// };
+
 class Solution {
-    void preorder(TreeNode* root, vector<int>& result) {
-        if (root == NULL) {
-            return;
-        }
-        result.push_back(root->val);
-        preorder(root->left, result);
-        preorder(root->right, result);
-    }
 
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+
+        if (root == NULL)
+            return {};
         vector<int> result;
-        preorder(root, result);
+        stack<TreeNode*> treeStack;
+        TreeNode* currNode;
+        treeStack.push(root);
+
+        while (treeStack.empty() == false) {
+            currNode = treeStack.top();
+            treeStack.pop();
+
+            result.push_back(currNode->val);
+
+            if (currNode->right != NULL)
+                treeStack.push(currNode->right);
+
+            if (currNode->left != NULL)
+                treeStack.push(currNode->left);
+        }
         return result;
     }
 };
