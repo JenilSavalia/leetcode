540b46d42ed889e447dfdbf6b2b7d4a0a8b7781a
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-16 22:32:42 +0530

    Time: 31 ms (59.89%) | Memory: 59.3 MB (75.34%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,7 +1,43 @@
 class Solution {
+    void heapify(vector<int>& a, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && a[left] > a[largest])
+            largest = left;
+
+        if (right < n && a[right] > a[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(a[i], a[largest]);
+            heapify(a, n, largest);
+        }
+    }
+
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        return nums[(nums.size()-1)-k+1];
+        // sort(nums.begin(),nums.end());
+        // return nums[(nums.size()-1)-k+1];
+
+        // we start from n/2-1 as n/2-1 is index of last node before leaf node,
+        // means next node is leaf node
+
+        // all leaf node are considered heap, there fore we start to heapify
+        // from n/2-1
+
+        int n = nums.size();
+        // Build MAX heap
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(nums, n, i);
+
+        // Extract max k-1 times
+        for (int i = n - 1; i >= n - k + 1; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+
+        return nums[0];
     }
 };
