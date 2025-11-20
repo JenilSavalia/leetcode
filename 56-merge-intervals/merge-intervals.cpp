58e29ba9657c7c57744239351a8d1e583b12c675
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-20 13:31:06 +0530

    Time: 3 ms (89.97%) | Memory: 23.9 MB (59.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -11,18 +11,14 @@ public:
         if (intervals.size() == 1)
             return intervals;
 
-        sort(intervals.begin(), intervals.end(),
-             [](vector<int> a, vector<int> b) {
-                 return a[0] < b[0]; // a comes before b if a is lesser than b
-             });
-
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> result;
         result.push_back(intervals[0]);
 
         int n = intervals.size();
         for (int i = 1; i < n; i++) {
             if (result.back()[1] >= intervals[i][0]) {
-                result.back()[1] = max(result.back()[1],intervals[i][1]);
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
             } else {
                 result.push_back(intervals[i]);
             }
