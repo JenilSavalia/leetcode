3778a8cb961e8ca1a950ccb97ee2a94e6ea8fab8
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-20 13:30:33 +0530

    Time: 131 ms (5.07%) | Memory: 52.7 MB (5.16%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    // return true if the first element should come before the second in the
+    // sorted order.
+    // bool compare(vector<int> a, vector<int> b) {
+    //     return a[0] < b[0]; // a comes before b if a is lesser than b
+    // }
+
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        if (intervals.size() == 1)
+            return intervals;
+
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int> a, vector<int> b) {
+                 return a[0] < b[0]; // a comes before b if a is lesser than b
+             });
+
+        vector<vector<int>> result;
+        result.push_back(intervals[0]);
+
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (result.back()[1] >= intervals[i][0]) {
+                result.back()[1] = max(result.back()[1],intervals[i][1]);
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
