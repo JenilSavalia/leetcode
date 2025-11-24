9eea0ffdde46a56a1404cacbceac1085dceafecf
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-24 14:15:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (27.68%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,103 +10,91 @@
  */
 
 using namespace std;
-// bool comp(ListNode* a, ListNode* b) { return a->val < b->val; }
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // vector<ListNode*> v;
 
-        // ListNode* temp1 = list1;
-        // while (temp1 != NULL) {
-        //     v.push_back(temp1);
-        //     temp1 = temp1->next;
+        // if (list1 == NULL && list2 != NULL) {
+        //     return list2;
+        // } else if (list1 != NULL && list2 == NULL) {
+        //     return list1;
         // }
 
-        // ListNode* temp2 = list2;
-        // while (temp2 != NULL) {
-        //     v.push_back(temp2);
-        //     temp2 = temp2->next;
-        // }
+        // ListNode* temp = NULL;
+        // ListNode* tempHead = NULL;
 
-        // sort(v.begin(), v.end(), comp);
-        // // list<int> sortedLL = {v[0]};
+        // while (list1 != NULL && list2 != NULL) {
+        //     if (list1->val > list2->val) {
+        //         if (temp == NULL) {
+        //             temp = list2;
+        //             tempHead = temp;
+        //             list2 = list2->next; // ADD THIS
+        //         } else {
+        //             temp->next = list2;
+        //             temp = temp->next;
+        //             list2 = list2->next;
+        //         }
+        //     } else if (list1->val < list2->val) {
+        //         if (temp == NULL) {
+        //             temp = list1;
+        //             tempHead = temp;
+        //             list1 = list1->next; // ADD THIS
 
-        // for (int i = 0; i < v.size(); i++) {
-        //     if (i + 1 < v.size()) {
-        //         v[i]->next = v[i + 1];
-        //     } else {
-        //         v[i]->next = NULL;
+        //         } else {
+        //             temp->next = list1;
+        //             temp = temp->next;
+        //             list1 = list1->next;
+        //         }
+        //     } else if (list1->val == list2->val) {
+        //         if (temp == NULL) {
+        //             temp = list1;
+        //             tempHead = temp;
+        //             list1 = list1->next;
+
+        //             temp->next = list2;
+        //             temp = temp->next;
+        //             list2 = list2->next;
+        //         } else {
+        //             temp->next = list1;
+        //             temp = temp->next;
+        //             list1 = list1->next;
+
+        //             temp->next = list2;
+        //             temp = temp->next;
+        //             list2 = list2->next;
+        //         }
         //     }
         // }
 
-        // return v.empty() ? NULL : v[0];
+        // if (list1) {
+        //     temp->next = list1;
+        // }
+        // if (list2) {
+        //     temp->next = list2;
+        // }
 
-        if (list1 == NULL && list2 != NULL) {
-            return list2;
-        } else if (list1 != NULL && list2 == NULL) {
-            return list1;
-        }
+        // return tempHead;
 
-        ListNode* temp = NULL;
-        ListNode* tempHead = NULL;
+        ListNode dummy;
+        ListNode* tail = &dummy;
 
         while (list1 != NULL && list2 != NULL) {
-            if (list1->val > list2->val) {
-                cout << "Running list1 > list2  " << list1->val << " "
-                     << list2->val << endl;
-                if (temp == NULL) {
-                    temp = list2;
-                    tempHead = temp;
-                    list2 = list2->next; // ADD THIS
-                } else {
-                    temp->next = list2;
-                    temp = temp->next;
-                    list2 = list2->next;
-                }
-            } else if (list1->val < list2->val) {
-                cout << "Running list1 < list2  " << list1->val << " "
-                     << list2->val << endl;
-                if (temp == NULL) {
-                    temp = list1;
-                    tempHead = temp;
-                    list1 = list1->next; // ADD THIS
-
-                } else {
-                    temp->next = list1;
-                    temp = temp->next;
-                    list1 = list1->next;
-                }
-            } else if (list1->val == list2->val) {
-                cout << "Running == else if  " << list1->val << " "
-                     << list2->val << endl;
-                if (temp == NULL) {
-                    temp = list1;
-                    tempHead = temp;
-                    list1 = list1->next;
-
-                    temp->next = list2;
-                    temp = temp->next;
-                    list2 = list2->next;
-                } else {
-                    temp->next = list1;
-                    temp = temp->next;
-                    list1 = list1->next;
-
-                    temp->next = list2;
-                    temp = temp->next;
-                    list2 = list2->next;
-                }
+            if (list1->val <= list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
             }
+            tail = tail->next;
         }
-
-        if (list1) {
-            temp->next = list1;
+        if (list1 != NULL) {
+            tail->next = list1;
         }
-        if (list2) {
-            temp->next = list2;
+        if (list2 != NULL) {
+            tail->next = list2;
         }
-
-        return tempHead;
+        return dummy.next;
     }
 };
