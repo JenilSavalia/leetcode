11da46d883d16cff4c0bc5d97edffc609e444594
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-25 12:24:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.03%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,19 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         if (nums.size() == 1)
             return;
-        int i =0;
+        int i = 0;
+        int j = 0;
 
         for (i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                // find next non zero element and replace with it
-                for (int j = i+1; j < nums.size(); j++) {
-                    if (nums[j] != 0) {
-                      swap(nums[i],nums[j]);
-                      break;    
-                    }
-                }
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
-
     }
 };
