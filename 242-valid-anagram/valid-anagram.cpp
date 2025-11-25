e73f45aedca7eaee3703d6db120f65db310640eb
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-11-25 13:39:40 +0530

    Time: 4 ms (37.68%) | Memory: 9.9 MB (25.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,40 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        // map<char, int> ss;
-
-        // bool isAnagram = true;
-
-        // for (int i = 0; i < s.length(); i++) {
-        //     ss[s[i]]++;
-        // }
-
-        // for (int i = 0; i < t.length(); i++) {
-        //     ss[t[i]]--;
-        // }
-
-        // for (auto x : ss) {
-        //     cout << x.first << " " << x.second << endl;
-        //     if (!x.second <= 0) {
-        //         isAnagram = false;
-        //         break;
-        //     }
-        // }
-
-        // return isAnagram;
-
-        map<char, int> ss;
-        map<char, int> tt;
-
-            if (s.length() != t.length()) {
+        if (s.length() != t.length())
             return false;
+
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+
+        for (char x : s) {
+            m1[x]++;
+        }
+        for (char x : t) {
+            m2[x]++;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            ss[s[i]]++;
-            tt[t[i]]++;
-        }
-
-        return ss==tt;
+        return m1 == m2;
     }
 };
