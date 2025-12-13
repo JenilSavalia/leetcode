b0fb76794daeed2f373ce33a2a99f0d9cf4232a4
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-12-13 22:48:13 +0530

    Time: 44 ms (76.25%) | Memory: 29.2 MB (46.35%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,18 +4,6 @@ public:
         vector<vector<int>> result;
         int n = nums.size();
 
-        // int i = 0;
-        // int j = 1;
-        // while (j != n - 1) {
-        //     int sum = nums[i] + nums[j];
-        //     for (int k = 0; k < n; k++) {
-        //         if ((i != j && j != k && j != k) && sum + nums[k] == 0) {
-        //             result.push_back({nums[i], nums[j], nums[k]});
-        //         }
-        //     }
-        //     i++;
-        //     j++;
-        // }
 
         sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
@@ -30,9 +18,11 @@ public:
             while (j < k) {
                 int twoSum = nums[j] + nums[k];
                 if (twoSum == -(curr)) {
-                    
-                    while(j<k && nums[j] == nums[j+1]) j++;
-                    while(k>j && nums[k] == nums[k-1]) k--;
+
+                    while (j < k && nums[j] == nums[j + 1])
+                        j++;
+                    while (k > j && nums[k] == nums[k - 1])
+                        k--;
                     result.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
@@ -47,7 +37,6 @@ public:
 
         // [-1,0,1,2,-1,-4]
         // -4 , -1 , -1 , 0 ,1 , 2
-
 
         //[2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10]
         // -10, -5, -5, -4, -4, -3, -2, -2, 0, 0, 1, 2, 2, 2, 2, 5, 5
