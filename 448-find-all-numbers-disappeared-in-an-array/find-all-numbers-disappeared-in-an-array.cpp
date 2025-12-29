8359ab3b348d6279e42ac246b85a2200d5fb3319
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2025-12-29 18:24:56 +0530

    Time: 2 ms (81.41%) | Memory: 53.2 MB (51.96%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        unordered_set<int> st;
+        // unordered_set<int> st;
 
-        for (int i = 0; i < nums.size(); i++) {
-            st.insert(nums[i]);
+        // for (int i = 0; i < nums.size(); i++) {
+        //     st.insert(nums[i]);
+        // }
+
+        // vector<int> rslt;
+
+        // for (int i = 1; i <= nums.size(); i++) {
+        //     if(st.find(i) == st.end()){
+        //         rslt.push_back(i);
+        //     }
+        // }
+
+        // return rslt;
+
+        // cyclic sort
+        int i = 0;
+        while (i < nums.size()) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums[i], nums[correct]);
+            } else {
+                i++;
+            }
         }
 
         vector<int> rslt;
-
-        for (int i = 1; i <= nums.size(); i++) {
-            if(st.find(i) == st.end()){
-                rslt.push_back(i);
+         i = 0;
+        while (i < nums.size()) {
+            if(nums[i] != i+1){
+                rslt.push_back(i+1);
             }
+            i++;
         }
 
         return rslt;
