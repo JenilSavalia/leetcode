76a75f6ed1c1575a038c46d1b55a764c0c93d74a
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-01 16:35:00 +0530

    Time: 4 ms (22.74%) | Memory: 14.4 MB (48.30%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -2,23 +2,45 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 
-        unordered_set<int> st1;
-        unordered_set<int> st2;
-        vector<int> inter;
+        // using two sets
+    
+        // unordered_set<int> st1;
+        // unordered_set<int> st2;
+        // vector<int> inter;
 
-        for (int num : nums1) {
-            st1.insert(num);
+        // for (int num : nums1) {
+        //     st1.insert(num);
+        // }
+        // for (int num : nums2) {
+        //     st2.insert(num);
+        // }
+
+        // for (int num : st1) {
+
+        //     if(st2.find(num) != st2.end()){
+        //         inter.push_back(num);
+        //     }
+        // }
+        // return inter;
+
+
+        // using bitset vector
+
+        const int MAX_VAL = 1001;
+        bitset<MAX_VAL> bs;
+        unordered_set<int> st;
+
+        for(int num : nums1){
+            bs.set(num);
         }
-        for (int num : nums2) {
-            st2.insert(num);
-        }
 
-        for (int num : st1) {
-
-            if(st2.find(num) != st2.end()){
-                inter.push_back(num);
+        for(int num : nums2){
+            if(bs.test(num)){
+                st.insert(num);
             }
         }
-        return inter;
+
+        return vector<int> (st.begin(),st.end());
+
     }
 };
