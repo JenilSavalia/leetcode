f191aad224a72095f2c0a5e6cec5e9229f331d45
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-02 19:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.83%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -2,24 +2,54 @@ class Solution {
 public:
     string reverseVowels(string s) {
 
-        vector<char> vovel;
+        // vector<char> vovel;
 
-        for (char chr : s) {
-            if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
-                chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
-                chr == 'O' || chr == 'U') {
-                vovel.push_back(chr);
+        // for (char chr : s) {
+        //     if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
+        //         chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
+        //         chr == 'O' || chr == 'U') {
+        //         vovel.push_back(chr);
+        //     }
+        // }
+        // reverse(vovel.begin(), vovel.end());
+
+        // int i =0;
+        // for (int j =0;j<s.length();j++) {
+        //     char chr = s[j];
+        //     if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
+        //         chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
+        //         chr == 'O' || chr == 'U') {
+        //         s[j] = vovel[i];
+        //         i++;
+        //     }
+        // }
+        // return s;
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            char chr = s[i];
+            char chr1 = s[j];
+            if ((chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
+                 chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
+                 chr == 'O' || chr == 'U') &&
+                (chr1 == 'a' || chr1 == 'e' || chr1 == 'i' || chr1 == 'o' ||
+                 chr1 == 'u' || chr1 == 'A' || chr1 == 'E' || chr1 == 'I' ||
+                 chr1 == 'O' || chr1 == 'U')) {
+                s[i] = chr1;
+                s[j] = chr;
+                i++;
+                j--;
             }
-        }
-        reverse(vovel.begin(), vovel.end());
-
-        int i =0;
-        for (int j =0;j<s.length();j++) {
-            char chr = s[j];
-            if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
-                chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
-                chr == 'O' || chr == 'U') {
-                s[j] = vovel[i];
+            else if ((chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' ||
+                 chr == 'u' || chr == 'A' || chr == 'E' || chr == 'I' ||
+                 chr == 'O' || chr == 'U') &&
+                !(chr1 == 'a' || chr1 == 'e' || chr1 == 'i' || chr1 == 'o' ||
+                  chr1 == 'u' || chr1 == 'A' || chr1 == 'E' || chr1 == 'I' ||
+                  chr1 == 'O' || chr1 == 'U')) {
+                j--;
+            } else {
                 i++;
             }
         }
