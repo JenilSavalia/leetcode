d9f2e71ccfa1aab5854c100b8349c8b38c7652e0
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-04 21:12:03 +0530

    Time: 11 ms (3.96%) | Memory: 19.3 MB (20.20%) - LeetSync

---
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        
+        sort(nums.begin(),nums.end());
+
+        nums[0] += k;
+        nums[nums.size()-1] -= k;
+
+        if(nums[nums.size()-1]  - nums[0] < 0){
+            return 0;
+        }else{
+            return nums[nums.size()-1]  - nums[0];
+        }
+
+    }
+};
