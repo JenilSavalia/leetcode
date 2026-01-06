02ffc2de19b2e6df4b9dd32958f4517cc6ae451a
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-06 11:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.19%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,29 +2,43 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int l = 0;
-        int r = nums.size() - 1;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                if (target >= nums[0]) {
-                    l = 0;
-                    r = i;
-                } else {
-                    l = i + 1;
-                    r = nums.size() - 1;
-                }
-                break;
-            }
-        }
+        // binary search it is baby....
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2; // To prevent potential overflow
-            if (nums[mid] > target) {
-                r = mid - 1;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
+        // ae we are giev a rotated array
+        // so when we find the mid, we would check which half is sorted
+        // is left half sorted i.e   nums[left] < nums[mid] // then right is
+        // unsorted if it is sorted we would check if out target liest b/w
+        // nums[left] and nums[mid-1] i.e  nums[left] <= target <= nums[mid-1]
+        // then we would whift our right pointer ; right = mid -1 i.e we are
+        // selecting left half
+
+        // if we are not able to find target in left range
+        // we would continue woth right range
+        // i.e left = mid+1
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
+            }
+            // check if left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } // if left half is not sorted then it means it has pivot and therefore right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
