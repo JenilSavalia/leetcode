b720da142f0ff6446c3f3045d7be274fdb921cac
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-10 13:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.19%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // return pow(x,0.5);
+        // if(x == 0) return 0;
 
-        if(x == 0) return 0;
+        // int ans = 1;
 
-        int ans = 1;
+        // for (int i = 1; i <= x; i++) {
+        //     if ((long long)i * i <= x) {
+        //         ans = i;
+        //     } else {
+        //         // i*i is more then x
+        //         break;
+        //     }
+        // }
+        // return ans;
 
-        for (int i = 1; i <= x; i++) {
-            if ((long long)i * i <= x) {
-                ans = i;
+        // doing binary serach
+
+        int l = 0;
+        int r = x;
+        int ans = 0;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if ((long long)mid*mid <= x) {
+                ans = mid;
+                l = mid + 1;
             } else {
-                // i*i is more then x
-                break;
+                r = mid - 1;
             }
         }
         return ans;
