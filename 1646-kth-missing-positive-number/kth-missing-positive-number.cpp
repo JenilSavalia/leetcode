b60615d6bec3094cf36e8ae0169b005e8d2921ea
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-01-13 10:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (90.30%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+
         if (arr[arr.size() - 1] == arr.size()) {
             return arr[arr.size() - 1] + k;
         }
@@ -9,17 +10,17 @@ public:
         int right = arr.size() - 1;
 
         while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            int mid = left + (right-left)/2;
-            int missingLeft = arr[mid] - (mid+1);
+            int missingLeft = arr[mid] - (mid + 1);
 
-            if (missingLeft >= k) {
-                right = mid-1;
-            }else{
-                left = mid+1;
+            if (missingLeft < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return right + 1 + k;
+        return left + k;
     }
 };
