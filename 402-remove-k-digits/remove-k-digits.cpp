e89fac379acca56fa2a118150f9419bacc482b8c
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-02-23 12:03:01 +0530

    Time: 7 ms (26.01%) | Memory: 14.1 MB (5.18%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -2,47 +2,81 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
 
-        if (num.length() == 0)
-            return "0";
-        if (num.length() == 1 && k > 0)
-            return "0";
+        // if (num.length() == 0)
+        //     return "0";
+        // if (num.length() == 1 && k > 0)
+        //     return "0";
 
-        string result = "";
+        // string result = "";
 
-        // we need to remove element which has number smaller to its next
-        int i = 0;
-        while (i < num.length() && k > 0) {
-            if (num[i] > num[i + 1]) {
+        // // we need to remove element which has number smaller to its next
+        // int i = 0;
+        // while (i < num.length() && k > 0) {
+        //     if (num[i] > num[i + 1]) {
+        //         k--;
+        //         num.erase(i, 1);
+        //         if (i > 0)
+        //             i--; // step back to recheck previous digit
+
+        //     } else {
+        //         i++;
+        //     }
+        // }
+
+        // // If k still remains, remove from end
+        // while (k > 0) {
+        //     num.pop_back();
+        //     k--;
+        // }
+
+        // i = 0;
+        // while (i < num.length()) {
+        //     if (num[i] == '0') {
+        //         i++;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // for (i; i < num.length(); i++) {
+        //     result += num[i];
+        // }
+
+        // if (result == "")
+        //     return "0";
+        // return result;
+
+        // stack approach
+
+        stack<int> stk;
+
+        for (char x : num) {
+
+            while (!stk.empty() && stk.top() > x && k > 0) {
                 k--;
-                num.erase(i, 1);
-                if (i > 0)
-                    i--; // step back to recheck previous digit
-
-            } else {
-                i++;
+                stk.pop();
             }
+            stk.push(x);
         }
 
-        // If k still remains, remove from end
+        // // If k still remains, remove from end
         while (k > 0) {
-            num.pop_back();
+            stk.pop();
             k--;
         }
 
-        i = 0;
-        while (i < num.length()) {
-            if (num[i] == '0') {
-                i++;
-            } else {
-                break;
-            }
-        }
-        for (i; i < num.length(); i++) {
-            result += num[i];
-        }
+        string result = "";
 
-        if (result == "")
-            return "0";
-        return result;
+        while (!stk.empty()) {
+            result += stk.top();
+            stk.pop();
+        }
+        reverse(result.begin(), result.end());
+        int i = 0;
+        while (i < result.length() && result[i] == '0') {
+            i++;
+        }
+        result = result.substr(i);
+
+        return result.empty() ? "0" : result;
     }
 };
