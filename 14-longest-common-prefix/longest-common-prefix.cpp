c0ca300b163140ab73698e230cb11cffe02a6bd9
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-02-23 17:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (29.59%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -2,29 +2,51 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
 
-        if (strs.empty())
-            return "";
-        if (strs.size() == 1)
-            return strs[0];
+        // vertical compare method
 
-        int minLen = INT_MAX;
-        for (string& x : strs) {
-            minLen = min(minLen, (int)x.length());
-        }
+        // if (strs.empty())
+        //     return "";
+        // if (strs.size() == 1)
+        //     return strs[0];
+
+        // int minLen = INT_MAX;
+        // for (string& x : strs) {
+        //     minLen = min(minLen, (int)x.length());
+        // }
+
+        // string result = "";
+
+        // for (int currIndex = 0; currIndex < minLen; currIndex++) {
+
+        //     char currentChar = strs[0][currIndex];
+
+        //     for (int i = 1; i < strs.size(); i++) {
+        //         if (strs[i][currIndex] != currentChar) {
+        //             return result;
+        //         }
+        //     }
+
+        //     result += currentChar;
+        // }
+
+        // return result;
+
+        // better approach
+
+        // sorting the array
+        sort(strs.begin(), strs.end());
+
+        int n = strs.size();
+        int point1 = 0;
+        int point2 = 0;
 
         string result = "";
 
-        for (int currIndex = 0; currIndex < minLen; currIndex++) {
-
-            char currentChar = strs[0][currIndex];
-
-            for (int i = 1; i < strs.size(); i++) {
-                if (strs[i][currIndex] != currentChar) {
-                    return result;
-                }
-            }
-
-            result += currentChar;
+        while ((point1 < strs[0].length() && point2 < strs[n - 1].length()) &&
+               strs[0][point1] == strs[n - 1][point2]) {
+            result += strs[0][point1];
+            point1++;
+            point2++;
         }
 
         return result;
