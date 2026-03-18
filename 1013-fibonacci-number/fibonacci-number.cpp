81202694072b2c924720a17070747df320e5a369
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-03-18 14:31:33 +0530

    Time: 14 ms (23.12%) | Memory: 7.8 MB (36.75%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int F(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return F(n-1) + F(n-2);
+    }
+
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return F(n);
+    }
+};
