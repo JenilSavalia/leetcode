7b9c3a4f8ae506ab166dd35202d598c947daa76e
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-04-07 19:01:48 +0530

    Time: 3 ms (69.66%) | Memory: 15.7 MB (49.65%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
 
-        vector<string> result;
-        generate("", n, result, 0, 0);
-        return result;
-    }
-    void generate(string str, int n, vector<string>& result, int openCnt,
-                  int closeCnt) {
-        if (str.size() == 2 * n) {
-            result.push_back(str);
-            return;
+    void generate(vector<string> &result,int open,int close,string curr,int n){
+
+        if(curr.length()==2*n){
+            result.push_back(curr);
         }
 
-        if (openCnt < n)
-            generate(str + "(", n, result, openCnt + 1, closeCnt);
+        if(open<n){
+            generate(result,open+1,close,curr+"(",n);
+        }
+        if(close<open){
+            generate(result,open,close+1,curr+")",n);
+        }
 
-        if (abs(openCnt - closeCnt))
-            // You can only add a closing parenthesis if there are more opens
-            // than closes so far.
-            generate(str + ")", n, result, openCnt, closeCnt + 1);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        generate(result,0,0,"",n);
+        return result;
     }
 };
