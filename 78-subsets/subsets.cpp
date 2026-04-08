3c3281a2109abd724e922c57c3bbb428528ffe37
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-04-08 17:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (30.07%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
+    void generate(vector<int> nums, vector<vector<int>>& result,
+                  vector<int> &subset, int index) {
 
-    void generate(vector<int> nums,vector<vector<int>> &result,vector<int> subset,int index){
+        // if(index >= nums.size()){
+        //     result.push_back(subset);
+        //     return;
+        // }
 
-        if(index >= nums.size()){
-            result.push_back(subset);
-            return;
+        // // pick
+        // subset.push_back(nums[index]);
+        // generate(nums,result,subset,index+1);
+
+        // // dont pick
+        // subset.pop_back();
+        // generate(nums,result,subset,index+1);
+
+        // for loop method
+
+        result.push_back(subset);
+
+        for (int i = index; i < nums.size(); i++) {
+            subset.push_back(nums[i]);
+            generate(nums, result, subset, i + 1);
+            subset.pop_back();
         }
-
-        // result.push_back(subset);
-
-        // pick
-        subset.push_back(nums[index]);
-        generate(nums,result,subset,index+1);
-
-        // dont pick
-        subset.pop_back();
-        generate(nums,result,subset,index+1);
-
-
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+
         vector<vector<int>> result;
         vector<int> subset;
 
-        generate(nums,result,subset,0);
+        generate(nums, result, subset, 0);
 
         return result;
-
     }
 };
