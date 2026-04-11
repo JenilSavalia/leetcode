8968ab3236ae402cfea905f42049ecaecbb72cb2
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-04-11 11:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (5.61%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp;
-        int i;
+        vector<int> res;
         if (nums.size() == 1)
             return;
         k = k % nums.size();
-        // If k is greater than the length of nums, then you're trying to rotate
-        // the array more times than its length — but logically, rotating an
-        // array by its length (n) results in the same array. So, rotating by k
-        // is the same as rotating by k % n.
-        for (i = nums.size() - k; i < nums.size(); i++) {
-            temp.push_back(nums[i]);
+
+        for (int i = nums.size() - k; i < nums.size(); i++) {
+            res.push_back(nums[i]);
         }
-        for (i = 0; i < nums.size() - k; i++) {
-            temp.push_back(nums[i]);
+        for (int i = 0; i < nums.size() - k; i++) {
+            res.push_back(nums[i]);
         }
-        for (i = 0; i < nums.size(); i++) {
-            nums[i] = temp[i];
+        for (int i = 0; i < nums.size(); i++) {
+            nums[i] = res[i];
         }
-        // return temp;
     }
 };
