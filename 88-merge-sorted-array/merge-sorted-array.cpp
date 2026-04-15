30dd72f5a4017c2ea8bedd2a44a2527ae3f0c99c
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-04-15 12:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.84%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,40 +2,66 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        vector<int> temp;
-        for (int i = 0; i < m; i++) {
-            temp.push_back(nums1[i]);
-        }
+        // vector<int> temp;
+        // for (int i = 0; i < m; i++) {
+        //     temp.push_back(nums1[i]);
+        // }
 
-        cout << temp.size();
+        // cout << temp.size();
 
-        int left = 0;
-        int right = 0;
+        // int left = 0;
+        // int right = 0;
 
-        int index = 0;
+        // int index = 0;
 
-        while (left < temp.size() && right < nums2.size()) {
+        // while (left < temp.size() && right < nums2.size()) {
 
-            if (temp[left] < nums2[right]) {
-                nums1[index] = temp[left];
-                left++;
-                index++;
+        //     if (temp[left] < nums2[right]) {
+        //         nums1[index] = temp[left];
+        //         left++;
+        //         index++;
+        //     } else {
+        //         nums1[index] = nums2[right];
+        //         right++;
+        //         index++;
+        //     }
+        // }
+
+        // while (left < temp.size()) {
+        //     nums1[index] = temp[left];
+        //     left++;
+        //     index++;
+        // }
+        // while (right < nums2.size()) {
+        //     nums1[index] = nums2[right];
+        //     right++;
+        //     index++;
+        // }
+
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int index = m + n - 1;
+
+        if (m == 0)
+            nums1 = nums2;
+
+        while (ptr1 >= 0 && ptr2 >= 0) {
+
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[index] = nums1[ptr1];
+                ptr1--;
+                index--;
             } else {
-                nums1[index] = nums2[right];
-                right++;
-                index++;
+                nums1[index] = nums2[ptr2];
+                ptr2--;
+                index--;
             }
         }
 
-        while (left < temp.size()) {
-            nums1[index] = temp[left];
-            left++;
-            index++;
-        }
-        while (right < nums2.size()) {
-            nums1[index] = nums2[right];
-            right++;
-            index++;
+        while (ptr2 >= 0) {
+            nums1[index] = nums2[ptr2];
+            ptr2--;
+            index--;
         }
     }
 };
