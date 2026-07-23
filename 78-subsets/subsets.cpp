0d7a32c5688bebc3654e75cf8d53045fe170cb6b
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-07-23 21:05:46 +0530

    Time: 7 ms (3.64%) | Memory: 17 MB (5.11%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    void generate(vector<int> nums, vector<vector<int>>& result,
-                  vector<int> &subset, int index) {
 
-        // if(index >= nums.size()){
-        //     result.push_back(subset);
-        //     return;
-        // }
-
-        // // pick
-        // subset.push_back(nums[index]);
-        // generate(nums,result,subset,index+1);
-
-        // // dont pick
-        // subset.pop_back();
-        // generate(nums,result,subset,index+1);
-
-        // for loop method
-
-        result.push_back(subset);
-
-        for (int i = index; i < nums.size(); i++) {
-            subset.push_back(nums[i]);
-            generate(nums, result, subset, i + 1);
-            subset.pop_back();
+    void backtrack(int index,vector<int> subset,vector<vector<int>> &result,vector<int> nums){
+        
+        if(index>=nums.size()){
+            result.push_back(subset);
+            return;
         }
+
+        // pick
+        subset.push_back(nums[index]);
+        backtrack(index+1,subset,result,nums);
+        subset.pop_back();
+
+        // dont pick
+        backtrack(index+1,subset,result,nums);
+
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-
+        
         vector<vector<int>> result;
-        vector<int> subset;
 
-        generate(nums, result, subset, 0);
 
+        backtrack(0,{},result,nums);
         return result;
+
     }
 };
