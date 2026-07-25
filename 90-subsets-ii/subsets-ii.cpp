ddc790ef092ab29bcf35d8a2946e71a5fe697eeb
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-07-25 14:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (51.20%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
+    void backtrack(vector<int>& nums, vector<int>& subset,
+                   vector<vector<int>>& result, int index) {
+
+        if (index >= nums.size()) {
+            result.push_back(subset);
+            return;
+        }
+
+        // include
+        subset.push_back(nums[index]);
+        backtrack(nums, subset, result, index + 1);
+
+        // exclude
+        subset.pop_back();
+
+        // duplicate subsets happedn when we include the element, which we
+        // excluded before
+
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1])
+            index++;
+        backtrack(nums, subset, result, index + 1);
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
 
         vector<vector<int>> result;
         vector<int> subset;
-        sort(nums.begin(), nums.end());
-        generate(nums, result, subset, 0);
+        int index = 0;
+
+        backtrack(nums, subset, result, index);
 
         return result;
-    }
-    void generate(vector<int>& nums, vector<vector<int>>& result,
-                  vector<int>& subset, int index) {
-
-        result.push_back(subset);
-
-        for (int i = index; i < nums.size(); i++) {
-
-            if (i > index && nums[i - 1] == nums[i]) {
-                continue;
-            }
-
-            subset.push_back(nums[i]);
-            generate(nums, result, subset, i + 1);
-            subset.pop_back();
-        }
     }
 };
