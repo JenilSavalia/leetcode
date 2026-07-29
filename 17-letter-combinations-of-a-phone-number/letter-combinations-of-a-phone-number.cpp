aaca85efe4815d2e7815ad73df28580c2565a4bd
Jenil Chandubhai Savalia <jenil.savalia.cd@gmail.com>
2026-07-29 21:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (90.06%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
+    void backtrack(string &digits,vector<string> &result,string &subset,int index,int limit,map<char,string> &mp) {
 
-        map<int, string> keypad;
-        keypad[2] = "abc";
-        keypad[3] = "def";
-        keypad[4] = "ghi";
-        keypad[5] = "jkl";
-        keypad[6] = "mno";
-        keypad[7] = "pqrs";
-        keypad[8] = "tuv";
-        keypad[9] = "wxyz";
-
-        vector<string> result;
-        string subset;
-
-        generate(digits, result, subset, 0, digits.length(), keypad);
-
-        return result;
-    }
-
-    void generate(string digits, vector<string>& result, string subset,
-                  int index, int limit, map<int, string>& keypad) {
-
-        if (subset.length() == limit) {
+        if(subset.size() == limit){
             result.push_back(subset);
             return;
         }
 
-        for (char x : keypad[digits[index] - '0']) {
-
+        for (char x : mp[digits[index]]) {
             subset.push_back(x);
-            generate(digits, result, subset,index+1, limit, keypad);
+            backtrack(digits,result,subset,index+1,limit,mp);
             subset.pop_back();
         }
+
+    }
+
+    vector<string> letterCombinations(string digits) {
+
+        map<char, string> mp;
+
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+
+        int index = 0;
+        int limit = digits.length();
+        string subset;
+        vector<string> result;
+
+        backtrack(digits,result,subset,index,limit,mp);
+
+        return result;
+
     }
 };
